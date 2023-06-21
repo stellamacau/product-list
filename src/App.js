86a27aa201ccf7eb6649403a44d25c30912cb3d3
stellamacau <134505408+stellamacau@users.noreply.github.com>
2023-06-21 20:56:01 +0800

    Change Theme Final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ListItem from "./ListItem";
 import SharedContext from "./SharedContext";
+import ThemeSwitch from "./ThemeSwitch";
 
 class App extends React.Component {
   state = {
     selectedItems: [],
+    theme: "dark",
+  };
+
+  toggleTheme = () => {
+    this.setState({
+      theme: this.state.theme === "dark" ? "light" : "dark",
+    });
   };
 
   listItemChangeHandler = (item) => {
@@ -26,39 +34,60 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="min-h-screen flex justify-center items-center bg-slate-200">
-        <div className="flex max-w-3xl mx-auto w-full gap-x-8">
-          <div className="w-1/2">
-            <h2 className="text-2xl font-bold">Apple Products</h2>
-            <ul className="mt-4 flex flex-col gap-y-3">
-              {["iPhone", "iPad", "MacBook", "Apple TV", "HomePod"].map(
-                (item) => {
-                  return (
-                    <ListItem
-                      key={item}
-                      active={this.state.selectedItems.includes(item)}
-                      onChange={this.listItemChangeHandler}
-                    >
-                      {item}
-                    </ListItem>
-                  );
-                }
-              )}
-            </ul>
-            <p className="mt-3 text-slate-400 text-sm">
-              {this.state.selectedItems.length} item(s) selected
-            </p>
-          </div>
-          <div className="w-1/2">
-            <h2 className="text-2xl font-bold text-slate-400">
-              Selected Products
-            </h2>
-            <p className="mt-4 text-slate-800 text-lg">
-              {this.state.selectedItems.join(", ")}
-            </p>
+      <SharedContext.Provider
+        value={{
+          theme: this.state.theme,
+          toggleTheme: this.toggleTheme,
+        }}
+      >
+        <div
+          className={`min-h-screen flex justify-center items-center  ${
+            this.state.theme === "dark" ? "bg-current" : "bg-slate-200"
+          }`}
+          //  className="min-h-screen flex justify-center items-center bg-slate-200"
+        >
+          <div className="flex max-w-3xl mx-auto w-full gap-x-8">
+            <div className="w-1/2">
+              <h2
+                className={`text-2xl font-bold ${
+                  this.state.theme === "dark" ? "text-white" : "text-black"
+                }`}
+              >
+                Apple Products
+              </h2>
+              <ul className="mt-4 flex flex-col gap-y-3">
+                {["iPhone", "iPad", "MacBook", "Apple TV", "HomePod"].map(
+                  (item) => {
+                    return (
+                      <ListItem
+                        title={item}
+                        active={this.state.selectedItems.includes(item)}
+                        onChange={this.listItemChangeHandler}
+                      >
+                        {item}
+                      </ListItem>
+                    );
+                  }
+                )}
+              </ul>
+              <p className="mt-3 text-slate-400 text-sm">
+                {this.state.selectedItems.length} item(s) selected
+              </p>
+            </div>
+            <div className="w-1/2">
+              <h2 className="text-2xl font-bold text-slate-400">
+                Selected Products
+              </h2>
+              <p className="mt-4 text-slate-800 text-lg">
+                {this.state.selectedItems.join(", ")}
+              </p>
+            </div>
+            <div>
+              <ThemeSwitch></ThemeSwitch>
+            </div>
           </div>
         </div>
-      </div>
+      </SharedContext.Provider>
     );
   }
 }
--- src/ListItem.js
+++ src/ListItem.js
@@ -2,20 +2,28 @@ import React from "react";
 import SharedContext from "./SharedContext";
 
 class ListItem extends React.Component {
+  toggleActive = () => {
+    this.props.onChange(this.props.title);
+  };
+
   render() {
     return (
-      <li
-        onClick={() => {
-          this.props.onChange(this.props.children);
+      <SharedContext.Consumer>
+        {(sharedData) => {
+          return (
+            <li
+              className={`px-6 py-4 rounded-lg shadow hover:cursor-pointer transition ${
+                this.props.active
+                  ? "bg-slate-800 text-white"
+                  : "bg-white hover:bg-slate-100"
+              }`}
+              onClick={this.toggleActive}
+            >
+              {this.props.title}
+            </li>
+          );
         }}
-        className={`px-6 py-4 rounded-lg shadow hover:cursor-pointer transition ${
-          this.props.active
-            ? "bg-slate-800 text-white"
-            : "bg-white hover:bg-slate-100"
-        }`}
-      >
-        {this.props.children}
-      </li>
+      </SharedContext.Consumer>
     );
   }
 }
